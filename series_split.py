4b285aced8b1bf9bf5f6364a3e49c9299770270a
huiping_guo <huiping.guo@dena.jp>
2022-08-02 22:32:54 +0900

    split修正

--- series_split.py
+++ series_split.py
@@ -8,14 +8,32 @@ import re
 target_path = ''
 rule_path = ''
 
+COMMON_MEDIA_EXTS = [
+    '.flv',
+    '.mkv',
+    '.mp4',
+    '.avi',
+    '.rmvb',
+    '.m2ts',
+    '.wmv',
+]
+
+# 字幕文件
+COMMON_CAPTION_EXTS = [
+    '.srt',
+    '.ass',
+    '.ssa',
+    '.sub',
+    '.smi',
+]
+
+exts = COMMON_MEDIA_EXTS + COMMON_CAPTION_EXTS
+
 ap = argparse.ArgumentParser()
 ap.add_argument('--path', required=True, help='目标路径')
-ap.add_argument('--rule', required=True, help='spilt rule')
 
 args = vars(ap.parse_args())
 target_path = args['path']
-rule_path = args['rule']
-
 
 def loop_dic(dic_path):
     for file_name in os.listdir(dic_path):
@@ -23,12 +41,49 @@ def loop_dic(dic_path):
         if os.path.isfile(path):
             if need_process_file(path):
                 rename_file(path)
+            else:
+                print("skip file",path)
 
 def need_process_file(path):
     file_name = os.path.basename(path)
-    pat = 'S(\d{1,4})E(\d{1,4}(\.5)?)'
+
+    ext = os.path.splitext(file_name)[1].lower()
+    if not ext in exts:
+        print("false1")
+        return False
+
+    pat = '[Ss](\d{1,4})[Ee](\d{1,4}(\.5)?)'
     res = re.match(pat, file_name)
-    return res is None
+    if res:
+        season = res[1]
+        print("season", season)
+        if season == 1:
+            return True
+        else:
+            return False
+    else:
+        print("false")
+        return True
+
+def find_file_index(path):
+    file_name = os.path.basename(path)
+    ex = os.path.splitext(file_name)
+    file_name = ex[0]
+    print("file name:", file_name)
+
+    pat = '[Ss](\d{1,4})[Ee](\d{1,4}(\.5)?)'
+    res = re.findall(pat, file_name)
+    if res:
+        print("res haha",res)
+        season, ep = res[0][0], res[0][1]
+        if int(season.strip()) == 1:
+            return int(ep.strip())
+
+    pat = '(\d{1,4})'
+    res = re.findall(pat, file_name)
+    if res:
+        print("res",res)
+        return int(res[0].strip())
 
 def get_season_ep(index):
     return get_real_season_ep(1,0,index)
@@ -47,18 +102,6 @@ def get_real_season_ep(season, count, index):
         return get_real_season_ep(season+1, season_ep_max ,index)
 
 
-def find_file_index(path):
-    file_name = os.path.basename(path)
-    ex = os.path.splitext(file_name)
-    file_name = ex[0]
-    print("file name:", file_name)
-
-    pat = '(\d{1,4})'
-    res = re.findall(pat, file_name)
-    if res:
-        print("res",res)
-        return int(res[0].strip())
-
 def rename_file(path):
     index = find_file_index(path)
     if not index:
@@ -71,6 +114,8 @@ def rename_file(path):
         return
     print("season, ep:", season, ep)
 
+    series = os.path.basename(os.path.dirname(path))
+
     dic_path = os.path.dirname(path)
     ex = path.split('.', 1)
     file_ex = ex[1]
@@ -80,20 +125,17 @@ def rename_file(path):
     season_str = get_int_str(season)
     ep_str = get_int_str(ep)
 
-    file_name = "S" + season_str + "E" + ep_str + "." + file_ex
+    file_name = series + " S" + season_str + "E" + ep_str + "." + file_ex
     new =  os.path.join(dic_path, file_name)
     print("new:", new)
 
     os.rename(path, new)
 
 def get_int_str(num):
-    if num < 10:
-        return "0" + str(num)
-    else:
-        return str(num)
+    return str(int(num)).zfill(2)
 
 def parse_rule_file():
-    file_path = rule_path
+    file_path = os.path.join(target_path,"rule.txt")
     if not file_path:
         return {}
     dic = {}
